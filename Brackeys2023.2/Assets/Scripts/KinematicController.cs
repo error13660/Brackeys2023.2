6613653506dec240df3f98992ae1ec6c240f1393
error <error13660@gmail.com>
2023-08-22 16:41:56 +0200

    Kinematic character controller

--- Brackeys2023.2/Assets/Scripts/KinematicController.cs
+++ Brackeys2023.2/Assets/Scripts/KinematicController.cs
@@ -14,6 +14,8 @@ public class KinematicController : MonoBehaviour
     private Vector3 lastPosition;
     private float lastFixedUpdate;
 
+    private float skinWidth = 0.02f;
+
     [Header("Mouse")]
     [SerializeField] private float mouseSensitivity;
     [SerializeField] private Transform head;
@@ -60,11 +62,8 @@ public class KinematicController : MonoBehaviour
         Vector3 movementVector = inputInFixedUpdate * velocity * Time.fixedDeltaTime;
         Vector3 newPosition;
 
-        GetMove(
-            out newPosition,
-            transform.position,
-            movementVector,
-            velocity * Time.deltaTime);
+        newPosition = transform.position + CollideAndSlide(movementVector, transform.position, 0, false);
+        newPosition = newPosition + CollideAndSlide(Vector3.down, newPosition, 0, true);
 
         lastPosition = nextPosition;
         nextPosition = newPosition;
@@ -72,74 +71,57 @@ public class KinematicController : MonoBehaviour
         lastFixedUpdate = Time.time;
     }
 
-    /// <summary>
-    /// Recursively calculates the new position of the player
-    /// </summary>
-    /// <param name="endPosition">The position where the palyer should be moved</param>
-    /// <returns>True if the calculation reached it's end</returns>
-    private bool GetMove(out Vector3 endPosition, Vector3 startPosition, Vector3 direction, float DistanceRemaining)
+    private Vector3 CollideAndSlide(Vector3 velocity, Vector3 position, int depth, bool isGravity)
     {
-        RaycastHit hit1;
+        if (depth >= 10) return Vector3.zero;
 
-        //raycast along direction
-        bool collision = Physics.CapsuleCast(
-            startPosition - Vector3.up * (height / 2),
-            startPosition + Vector3.up * (height / 2),
+        float dist = velocity.magnitude + skinWidth;
+
+        RaycastHit hit;
+        if (Physics.SphereCast(
+            position,
             radius,
-            direction,
-            out hit1,
-            DistanceRemaining
-            );
-
-        Vector3 point1;
-        if (collision)
-        {
-            if (hit1.distance > 0.01f)
-            {
-                point1 = startPosition + direction * hit1.distance;
-            }
-            else
-            {
-                point1 = startPosition;
-            }
-            DistanceRemaining -= hit1.distance;
-        }
-        else
-        {
-            point1 = startPosition + direction * velocity /* Time.fixedDeltaTime*/;
-            DistanceRemaining -= velocity/* * Time.fixedDeltaTime*/;
-        }
-
-        RaycastHit hit2;
-        Vector3 point2 = point1;
-        Debug.DrawRay(hit1.point, Vector3.up, Color.red);
-
-        //gravity
-        //gravity always happens (it might not do anything)
-        if (Physics.CapsuleCast(
-            point1 - Vector3.up * (height / 2),
-            point1 + Vector3.up * (height / 2),
-            radius,
-            Vector3.down,
-            out hit2
+            velocity.normalized,
+            out hit,
+            dist
             ))
         {
-            point2 = point1 + Vector3.down * hit2.distance;
-        }
+            Vector3 snapToSurface = velocity.normalized * (hit.distance - skinWidth);
+            Vector3 leftover = velocity - snapToSurface;
+            float angle = Vector3.Angle(Vector3.up, hit.normal);
 
-        if (collision) //if a wall was hit
+            if (snapToSurface.magnitude <= skinWidth) //discard small movements from inaccuracy
+            {
+                snapToSurface = Vector3.zero;
+            }
+
+            if (angle <= 40)
+            {
+                if (isGravity)
+                {
+                    return snapToSurface;
+                }
+                leftover = ProjectAndScale(leftover, hit.normal);
+            }
+            //wall or steep slope
+            else
+            {
+                float scale = 1 - Vector3.Dot(
+                    new Vector3(hit.normal.x, 0, hit.normal.z).normalized,
+                    -new Vector3(velocity.x, 0, velocity.z).normalized);
+                leftover = ProjectAndScale(leftover, hit.normal) * scale;
+            }
+
+            return snapToSurface + CollideAndSlide(leftover, position + snapToSurface, depth + 1, isGravity);
+        }
+        return velocity;
+
+        Vector3 ProjectAndScale(Vector3 leftover, Vector3 normal)
         {
-            endPosition = point2;
-            return false;
+            float mag = leftover.magnitude;
+            leftover = Vector3.ProjectOnPlane(leftover, normal).normalized;
+            leftover *= mag;
+            return leftover;
         }
-
-        if (DistanceRemaining <= 0) //if the distance was reached
-        {
-            endPosition = startPosition + direction * (velocity/*Time.fixedDeltaTime*/);
-            return false;
-        }
-
-        endPosition = startPosition;
-        return true;
     }
 }
